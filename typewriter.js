c882ef25da7a62ae9468fbc1f9fd94b03ed1c1ec
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-09 15:59:25 +0100

    commands added and typewriter works

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,18 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", clearHTML);
+window.addEventListener("DOMContentLoaded", cleartheHTML);
 
+//get the text
 const getText = document.querySelector(".typewritten").textContent;
+
+//the variable n = 0
 let n = 0;
 
 //run clearHTML
-function clearHTML() {
-  console.log("clearHTML");
+function cleartheHTML() {
+  console.log("cleartheHTML");
+
+  //this is clearing the HTML
   document.querySelector(".typewritten").textContent = "";
 
   loop();
@@ -17,8 +22,11 @@ function clearHTML() {
 function loop() {
   console.log("loop");
 
+  //Length makes sure that every letter is added
   if (n < getText.length) {
+    //Because n = 0, the variable getText starts at n, which is 0 (no letters in the beginning until you click the button)
     document.querySelector(".typewritten").textContent += getText.charAt(n);
+    //After clicking the button, it adds a letter one by one
     n++;
     setTimeout(loop, 150);
   }
